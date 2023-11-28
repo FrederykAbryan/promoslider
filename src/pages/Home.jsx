846bff4a1d8f84f3ba32c972f12e9a39c7b10e16
Frederyk Abryan <fabryan03@gmail.com>
2023-11-28 15:23:34 +0800

    change image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,30 +7,30 @@ const Home = () => {
       <div className="row d-flex justify-content-center">
         <div className="col col-lg-6 d-flex justify-content-center">
           <Card style={{ width: "18rem", alignItems:"center" }}>
-            <Card.Img className="img-fluid" variant="top" src="https://www.btn.co.id/-/media/User-Defined/Identitas-Perusahaan/02_BTN_MasterBrand.jpg?la=id" />
+            <Card.Img className="img-fluid" variant="top" src="https://www.svgrepo.com/download/92070/discount.svg" />
             <Card.Body>
-              <Card.Title>Promo</Card.Title>
+              {/* <Card.Title>Promo</Card.Title> */}
               {/* <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the content.
               </Card.Text> */}
               <NavLink to={"/promo"}>
-                <Button variant="primary">Go</Button>
+                <Button variant="primary" size="lg">Promo</Button>
               </NavLink>
             </Card.Body>
           </Card>
         </div>
         <div className="col col-lg-6 d-flex justify-content-center">
           <Card style={{ width: "18rem", alignItems:"center" }}>
-            <Card.Img variant="top" src="https://www.btn.co.id/-/media/User-Defined/Identitas-Perusahaan/02_BTN_MasterBrand.jpg?la=id" />
+            <Card.Img variant="top" src="https://www.svgrepo.com/download/112870/deal.svg" />
             <Card.Body>
-              <Card.Title>Program</Card.Title>
+              {/* <Card.Title>Program</Card.Title> */}
               {/* <Card.Text>
                 Some quick example text to build on the card title and make up
                 the bulk of the content.
               </Card.Text> */}
               <NavLink to={"/program"}>
-                <Button variant="primary">Go</Button>
+                <Button variant="primary" size="lg">Program</Button>
               </NavLink>
             </Card.Body>
           </Card>
